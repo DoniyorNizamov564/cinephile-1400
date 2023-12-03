dafe7b9a7ab6e2df8c10ba24e707d04263e11168
doniyor <abc@ssd.ru>
2023-12-03 16:35:41 +0300

    first commit

---
+++ assets/Films-d883d7ac.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ListContent-4f305e90.js";import{o as a,c as o,a as s}from"./index-09915be5.js";const t={class:"main"},i={__name:"Films",setup(c){return(_,m)=>(a(),o("main",t,[s(e,{type:"movie"})]))}};export{i as default};
